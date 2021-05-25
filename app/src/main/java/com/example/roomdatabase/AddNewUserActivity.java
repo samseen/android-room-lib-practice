9162c9dc6edae9f23708960ff22d7c0a86f834bc
Samson Akanbi <samson.akanbi@outlook.com>
2021-05-25 21:40:56 +0100

    Completed the add new user layout.

---
+++ app/src/main/java/com/example/roomdatabase/AddNewUserActivity.java
@@ -0,0 +1,14 @@
+package com.example.roomdatabase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddNewUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_user);
+    }
+}
