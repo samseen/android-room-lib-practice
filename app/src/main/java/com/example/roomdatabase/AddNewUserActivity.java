e1985b86affe0b8a15ab9a84331fcda76c2a307a
Samson Akanbi <samson.akanbi@outlook.com>
2021-05-25 23:02:12 +0100

    Completed the project.

--- app/src/main/java/com/example/roomdatabase/AddNewUserActivity.java
+++ app/src/main/java/com/example/roomdatabase/AddNewUserActivity.java
@@ -3,6 +3,12 @@ package com.example.roomdatabase;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.roomdatabase.db.AppDatabase;
+import com.example.roomdatabase.db.User;
 
 public class AddNewUserActivity extends AppCompatActivity {
 
@@ -10,5 +16,28 @@ public class AddNewUserActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_user);
+
+        EditText firstNameInput = findViewById(R.id.first_name_input);
+        EditText lastNameInput = findViewById(R.id.last_name_input);
+        Button saveButton = findViewById(R.id.save_button);
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveNewUser(firstNameInput.getText().toString(), lastNameInput.getText().toString());
+            }
+        });
+    }
+
+    private void saveNewUser(String firstName, String lastName) {
+        AppDatabase db = AppDatabase.getDbInstance(this.getApplicationContext());
+
+        User user = new User();
+        user.firstName = firstName;
+        user.lastName = lastName;
+        db.userDao().insertUser(user);
+
+        finish();
+
     }
 }
--- app/src/main/java/com/example/roomdatabase/MainActivity.java
+++ app/src/main/java/com/example/roomdatabase/MainActivity.java
@@ -1,14 +1,70 @@
 package com.example.roomdatabase;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.roomdatabase.db.AppDatabase;
+import com.example.roomdatabase.db.User;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private UserListAdapter userListAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button addNewUserButton = findViewById(R.id.add_new_user_button);
+        addNewUserButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivityForResult(new Intent(MainActivity.this,
+                        AddNewUserActivity.class), 100);
+            }
+        });
+
+        initRecyclerView();
+
+        loadUserList();
+    }
+
+    private void initRecyclerView() {
+        RecyclerView recyclerView = findViewById(R.id.recyclerview);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration
+                (this, DividerItemDecoration.HORIZONTAL);
+        recyclerView.addItemDecoration(dividerItemDecoration);
+
+        userListAdapter = new UserListAdapter(this);
+
+        recyclerView.setAdapter(userListAdapter);
+    }
+
+    private void loadUserList() {
+        AppDatabase db = AppDatabase.getDbInstance(this.getApplicationContext());
+        List<User> userList =  db.userDao().getAllUsers();
+        userListAdapter.setUserList(userList);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(requestCode == 100) {
+
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
