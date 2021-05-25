a4c01e93ac806137b6bb6b2fb55d4505d8933d8b
Samson Akanbi <samson.akanbi@outlook.com>
2021-05-25 13:16:13 +0100

    Completed the User DAO

--- app/src/main/java/com/example/roomdatabase/db/UserDao.java
+++ app/src/main/java/com/example/roomdatabase/db/UserDao.java
@@ -1,4 +1,21 @@
 package com.example.roomdatabase.db;
 
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
 public interface UserDao {
+
+    @Query("SELECT * FROM user")
+    List<User> getAllUsers();
+
+    @Insert
+    void insertUser(User... users);
+
+    @Delete
+    void delete(User user);
 }
